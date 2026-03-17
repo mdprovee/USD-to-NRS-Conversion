cef312222797554f8677197430edb775e8e761fa
mdprovee <mprovencher@csumb.edu>
2026-03-17 15:45:39 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-#USD to NRS
-#Conversion app
 import streamlit as st
-Amount = input("What amount do you want to convert (in USD)?")
-TotalConversion = float(Amount)*144
-print("The total amount in NRS is " + str(TotalConversion))
+
+st.title("USD to NRS Converter")
+
+amount = st.text_input("What amount do you want to convert (in USD)?")
+
+if amount:
+    try:
+        total_conversion = float(amount) * 144
+        st.write("The total amount in NRS is:", total_conversion)
+    except ValueError:
+        st.error("Please enter a valid number.")
